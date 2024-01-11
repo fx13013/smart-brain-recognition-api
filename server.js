ebdae56f5149cfcc6b9afc4720135b2a63e9ec97
fx13013 <r_carlos013@hotmail.com>
2024-01-11 18:16:51 +0100

    Config database connection

--- server.js
+++ server.js
@@ -8,14 +8,27 @@ const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 
+// const db = knex({
+//   client: "pg",
+//   connection: {
+//     host: "127.0.0.1",
+//     port: 5432,
+//     user: "postgres",
+//     password: "sql13013*",
+//     database: "smart-brain",
+//   },
+// });
+
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    connectString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    host: process.env.DATABASE_HOST,
     port: 5432,
-    user: "postgres",
-    password: "sql13013*",
-    database: "smart-brain",
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB,
   },
 });
 
@@ -43,5 +56,6 @@ app.put("/image", (req, res) => image.handleImage(req, res, db));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  console.log(process.env);
   console.log(`APP IS RUNNING ON PORT ${PORT}!!!"`);
 });
